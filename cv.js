27b46a30c0404ea39893d889e001434e2ad6ac37
RobinVaesenPXL <11901804@student.pxl.be>
2024-12-22 22:26:13 +0100

    WEB-08 Added new design

--- cv.js
+++ cv.js
@@ -1,4 +1,12 @@
+const cardToggles = document.querySelectorAll(".card-toggle");
+
 function topFunction() {
-  document.body.scrollTop = 0; // Safari
-  document.documentElement.scrollTop = 0; // Chrome, Firefox, IE and Opera
-}
+    document.body.scrollTop = 0; // Safari
+    document.documentElement.scrollTop = 0; // Chrome, Firefox, IE and Opera
+  }  
+
+cardToggles.forEach((button) => {
+    button.addEventListener("click", () => {
+        button.parentNode.classList.toggle("open");
+    })
+})
